70dee07c8c5b2bb3a3dc5b37e005ad1121bb2143
João Danielewicz <joao.wicz@gmail.com>
2024-10-25 09:33:23 -0300

    commit

--- AulaEntityFramework/AulaEntityFramework/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AulaEntityFramework.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
